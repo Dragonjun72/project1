2b71f0184cc37be07fe921e24da7518b7d15a40d
Dragonjun72 <91768481+Dragonjun72@users.noreply.github.com>
2021-10-02 12:07:49 +0900

    Add files via upload

---
+++ condition.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+
+
+int main_condition(void)
+{
+	/*int age = 25;
+	if (age >= 20)
+	{
+		printf("일반인입니다.\n");
+	}
+	else
+	{
+		printf("학생입니다.\n");
+	}
+	*/
+	
+	/*int age = 25;
+	if (age >=8 && age <=13 )
+	{
+		printf("초등학생입니다.\n");
+	}
+	else if (age >= 14 && age <=16)
+	{
+		printf("중학생입니다.\n");
+	}
+	else if (age >=17 && age <=19 )
+	{
+		printf("고등학생입니다.\n");
+	}
+	else 
+	{
+		printf("어른입니다.\n");
+	}*/
+
+	/*for (int i = 1; i <= 30; i++)
+	{
+		if(i>=6)
+		{
+			printf("나머지는 집에 가세요.\n");
+			break;
+		}
+		printf("%d번 학생은 발표를 준비하세요.\n", i);
+	}*/
+
+	/*for (int i = 1; i <= 30; i++)
+	{
+		if (i >= 6 && i <= 10)
+		{
+			if (i == 7)
+			{
+				printf("%d번은 결석입니다.\n", i);
+				continue;
+			}
+			printf("%d번은 발표를 준비하세요.\n", i);
+		}
+	}*/
+
+
+	return (0);
+
+}
