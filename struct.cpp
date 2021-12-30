35afaeb08e8a6b522779800595f328cd2db0e79b
Dragonjun72 <91768481+Dragonjun72@users.noreply.github.com>
2021-12-30 15:46:48 +0900

    Add files via upload

--- struct.cpp
+++ struct.cpp
@@ -7,9 +7,21 @@ struct GameInfo {
 	int year;
 	int price;
 	char * company;
+
+	struct GameInfo * friendGame; // 연관업체 게임
 };
 
-int main_strcut(void)
+typedef struct {
+
+	char * name;
+	int year;
+	int price;
+	char * company;
+
+	struct GameInfo * friendGame; // 연관업체 게임
+} GAME_INFO;
+
+int main_struct1(void)
 {
 	// [게임 출시]
 	// 이름 : 나도게임
@@ -61,23 +73,57 @@ int main_strcut(void)
 	struct GameInfo gameArray[2] = {
 		{"나도게임",2017,50,"나도회사"} ,
 		{"너도게임",2017,100,"너도회사"}
-
 	};
 	
 	// 구조체 포인터
 	struct GameInfo * gamePtr; // 미션맨
 	gamePtr = &gameinfo1;
 	printf("\n\n-- 미션맨의 게임 출시 정보 -- \n");
-/*
-	printf("  게임명   : %s\n", (*gamePtr).name); // *ptr 
+	printf("  게임명   : %s\n", (*gamePtr).name); //*ptr 
 	printf("  발매년도 : %d\n", (*gamePtr).year);
 	printf("  가격     : %d\n", (*gamePtr).price);
 	printf("  제작사   : %s\n", (*gamePtr).company);
-*/
+
 	printf("  게임명   : %s\n", gamePtr -> name); // *ptr 
 	printf("  발매년도 : %d\n", gamePtr -> year);
 	printf("  가격     : %d\n", gamePtr->price);
 	printf("  제작사   : %s\n", gamePtr->company);
+
+
+	// 연관 업체 게임 소개
+	gameinfo2.friendGame = &gameinfo1;
+		printf("\n\n-- 연관 업체의 게임 출시 정보 -- \n");
+	
+	printf("  게임명   : %s\n", gameinfo2.friendGame-> name); // *ptr 
+	printf("  발매년도 : %d\n", gameinfo2.friendGame -> year);
+	printf("  가격     : %d\n", gameinfo2.friendGame->price);
+	printf("  제작사   : %s\n", gameinfo2.friendGame->company);
+	
+	// typedef
+	// 자료형에 별명 지정 
+	int i = 1;
+	typedef int 정수;
+	typedef float 실수;
+
+	정수 정수변수 = 3;
+	실수 실수변수 = 3.23f;
+
+	printf("\n\n정수변수 : %d, 실수변수 : %.2f\n\n", 정수변수, 실수변수);
+
+	typedef struct GameInfo 게임정보;
+	게임정보 game1;
+	game1.name = "한글 게임";
+	game1.year = 2015;
+
+	GAME_INFO game2;
+	game2.name = "한글 게임2";
+	game2.year = 2014;
+
+/*
+	struct GameInformation game3;
+	game3.name = "한글 게임3";
+	game3.year = */
+
 
 	return 0;
 }
